1d568cebd63953f768a86971045d64a052892231
Janar Juusu <janar.juusu@gmail.com>
2017-03-26 13:10:00 +0300

    correct jquery failover and minor html tweaks/fixes

--- js/failover.js
+++ js/failover.js
@@ -1,2 +1,1 @@
 window.jQuery || document.write('<script src="js/jquery-3.1.1.min.js"></script>');
-$.fn.modal || document.write('<script src="js/bootstrap-3.3.7.min.js"></script>');
--- scripts/failover.js
+++ scripts/failover.js
@@ -1,1 +1,1 @@
-window.jQuery || document.write('<script src="scripts/jquery-3.1.1.min.js">\x3C/script>');
+window.jQuery || document.write('<script src="js/jquery-3.1.1.min.js">\x3C/script>');
