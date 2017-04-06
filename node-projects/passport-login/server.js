94b73f0cc205eb99cd40c2539a79b0ef3bf530c0
Janar Juusu <janar.juusu@gmail.com>
2017-04-06 22:55:04 +0300

    some bug fixing

--- node-projects/passport-login/server.js
+++ node-projects/passport-login/server.js
@@ -15,6 +15,12 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(session({
 	secret: 'thisisabigsecret',
 	resave: true,
