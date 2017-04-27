3b509ed15753aec2d61c8699ffb58e50da578208
Cardo Kambla <kamblaca@gmail.com>
2017-04-27 16:27:42 +0300

    Big floating number problem fix

--- scripts/picker.js
+++ scripts/picker.js
@@ -73,4 +73,4 @@ var MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
 function get_age(time){
     var date_array = time.split('-')
     var years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
-    return years_elapsed; }
+    return Math.floor(years_elapsed); }
