0deed6041fa034af06b3156807b1f124981804b5
Cardo Kambla <kamblaca@gmail.com>
2017-04-27 16:38:45 +0300

    There was a missing semicolon

--- scripts/picker.js
+++ scripts/picker.js
@@ -71,6 +71,6 @@ function sendLike(){
 
 var MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
 function get_age(time){
-    var date_array = time.split('-')
+    var date_array = time.split('-');
     var years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
     return Math.floor(years_elapsed); }
