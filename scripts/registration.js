8cf7f39eeac15cf111500abeeaf486a0bed75cbe
Cardo Kambla <kamblaca@gmail.com>
2017-04-27 16:55:52 +0300

    Registration fix attempt

--- scripts/registration.js
+++ scripts/registration.js
@@ -119,7 +119,8 @@ function validateBirthDate() {
 
 var MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
 function get_age(time){
-    var date_array = time.split('-');
+    var stringTime = String(time);
+    var date_array = stringTime.split('-');
     var years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
     return Math.floor(years_elapsed); }
 function validateGender() {
