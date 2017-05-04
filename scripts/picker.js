3ae5433ed645617ac749aea2059a2c2df0688ed6
Cardo Kambla <kamblaca@gmail.com>
2017-05-04 17:08:51 +0300

    Added accept and rejects GET

--- scripts/picker.js
+++ scripts/picker.js
@@ -61,7 +61,18 @@ function sendDislike(){
     console.log("Dislike button clicked");
     $("#profilePicture").attr("src","../../pictures/loading.gif");
     $.ajax({
-        url: 'https://iizibang.jjdev.eu/api/rejectmatch',
+        url: 'https://iizibang.jjdev.eu/api/acceptmatch',
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    });
+    $.ajax({
+        url: 'https://iizibang.jjdev.eu/api/findmatches',
         type: 'GET',
         contentType: "application/json; charset=utf-8",
         success: function(data) {
@@ -89,7 +100,18 @@ function sendLike(){
     console.log("Like button clicked");
     $("#profilePicture").attr("src","../../pictures/loading.gif");
     $.ajax({
-        url: 'https://iizibang.jjdev.eu/api/acceptmatch',
+        url: 'https://iizibang.jjdev.eu/api/rejectmatch',
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    });
+    $.ajax({
+        url: 'https://iizibang.jjdev.eu/api/findmatches',
         type: 'GET',
         contentType: "application/json; charset=utf-8",
         success: function(data) {
