08b74c642ecf61dfecc6bc63038e8dadc49c4484
Janar Juusu <janar.juusu@gmail.com>
2017-05-06 15:39:17 +0300

    improve code readability - registration.js

--- scripts/registration.js
+++ scripts/registration.js
@@ -28,7 +28,7 @@ function getFormData(form){
     var indexed_array = {};
 
     $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
+        indexed_array[n.name] = n.value;
     });
 
     return indexed_array;
@@ -36,7 +36,7 @@ function getFormData(form){
 
 function validateUserName(){
     var x = document.forms["reg-form"]["username"].value.trim();
-    if (x == "") {
+    if (x) {
         alert("Username must be filled out");
         return false;
     }
@@ -50,7 +50,7 @@ function validatePassword() {
     var re = (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
     var x = document.forms["reg-form"]["password"].value.trim();
     var y = document.forms["reg-form"]["passwordConfirm"].value.trim();
-    if (x == "") {
+    if (x) {
         alert("Password must be filled out");
         return false;
     }
@@ -64,12 +64,13 @@ function validatePassword() {
     }
     return true;
 }
+
 function validateEmail() {
     // Refrence : http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var x = document.forms["reg-form"]["email"].value.trim();
     var y = document.forms["reg-form"]["emailConfirm"].value.trim();
-    if (x == "") {
+    if (x) {
         alert("Email must be filled out");
         return false;
     }
@@ -83,22 +84,25 @@ function validateEmail() {
     }
     return true;
 }
+
 function validateFirstName() {
     var x = document.forms["reg-form"]["firstname"].value.trim();
-    if (x == "") {
+    if (x) {
         alert("First name must be filled out");
         return false;
     }
     return true;
 }
+
 function validateLastName() {
     var x = document.forms["reg-form"]["lastname"].value.trim();
-    if (x == "") {
+    if (x) {
         alert("Surname must be filled out");
         return false;
     }
     return true;
 }
+
 function validateBirthDate() {
     var birthAge = document.getElementById("birthdate").value;
     if (birthAge === ""){
@@ -117,10 +121,12 @@ function validateBirthDate() {
 }
 
 var MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
-function get_age(time){
+function get_age(time) {
     var date_array = time.split('-');
     var years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
-    return Math.floor(years_elapsed); }
+    return Math.floor(years_elapsed);
+}
+
 function validateGender() {
     var x = document.forms["reg-form"]["gender"].value;
     if (x === "Male" || x === "Female" || x === "Other") {
@@ -129,6 +135,7 @@ function validateGender() {
     alert("Gender must be chosen");
     return false;
 }
+
 function validateInterest() {
     var x = document.forms["reg-form"]["interest"].value;
     if (x === "Men" || x === "Women" || x === "Other") {
@@ -154,10 +161,9 @@ $('#registerButton').on('click', function (e) {
 
     var formData = getFormData($("#reg-form"));
     console.log(formData);
-    if (
-        validateUserName() === true && validatePassword() === true && validateEmail() === true &&  validateFirstName() === true && validateLastName() === true && validateBirthDate() === true &&
+    if (validateUserName() && validatePassword() && validateEmail() && validateFirstName() && validateLastName() && validateBirthDate() &&
         validateGender() && validateInterest()
-        ) {
+       ) {
 
     $.ajax({
         url: 'https://iizibang.jjdev.eu/api/register',
@@ -177,23 +183,5 @@ $('#registerButton').on('click', function (e) {
             console.log(data);
         }
     });
-
-    var imageForm = new FormData();
-    /*$.ajax({
-        url: 'https://iizibang.jjdev.eu/api/uploadImage',
-        type: 'POST',
-        contentType: "multipart/form-data; charset=utf-8",
-
-        data: JSON.stringify(formData),
-        success: function(data) {
-            window.location.replace("https://iizibang.jjdev.eu/login");
-            console.log(data);
-
-        },
-        error: function (data) {
-            alert('Error');
-            console.log(data);
-        }
-    });*/
 }
 });
