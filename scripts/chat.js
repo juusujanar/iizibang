dd7cde8b9b280f6becf838b8aa24ad97cea8fb21
Cardo Kambla <kamblaca@gmail.com>
2017-05-06 11:04:06 +0300

    Chat box expanding and collapsing works

--- scripts/chat.js
+++ scripts/chat.js
@@ -20,12 +20,24 @@ $(document).ready(function() {
         }
     });
     for (var i = 0;i<10;i++){
+    /*<div class="chatBox">
+            <div class="chatBoxRow">
+            <img src="../../pictures/no_pic.jpg" class="pictureOfInterest" alt="pictureOfInterest">
+            <div class="interestInfo">
+            <div class="interestName"><p class="chatText">Username</p></div>
+            <div class="interestLastMessage"><p class="chatText">How you doing?</p></div>
+        </div>
+        </div>
+        <button class="expandButton" id="btnExpand"onclick="doSomething();" /><i class="down"></i></button>
+            </div>*/
     // <div class="chatBox">
         var chatBox = document.createElement("DIV");
         chatBox.setAttribute("class", "chatBox");
         chatBox.setAttribute('data-id', i.toString());
         //chatBox.setAttribute("onClick", "openChat()");
     //         <img src="../../pictures/no_pic.jpg" class="pictureOfInterest" alt="pictureOfInterest">
+        var chatBoxRow = document.createElement("DIV");
+        chatBoxRow.setAttribute("class","chatBoxRow")
         var image = document.createElement("IMG");
         image.setAttribute("src","../../pictures/no_pic.jpg");
         image.setAttribute("class","pictureOfInterest");
@@ -44,13 +56,22 @@ $(document).ready(function() {
         var interestLastMessage = document.createElement("DIV");
         interestLastMessage.setAttribute("class","interestLastMessage");
         var chatText = document.createElement("P");
+        interestLastMessage.appendChild(chatText);
         chatText.setAttribute("class","chatText");
         chatText.innerHTML = "How you doing?";
-        interestLastMessage.appendChild(chatText);
+        var expandButton = document.createElement("BUTTON");
+        expandButton.setAttribute("class","expandButton")
+        var downArrow = document.createElement("I");
+        downArrow.setAttribute("class","down");
         interestInfo.appendChild(interestName);
         interestInfo.appendChild(interestLastMessage);
-        chatBox.appendChild(image);
-        chatBox.appendChild(interestInfo);
+        chatBoxRow.appendChild(image);
+        chatBoxRow.appendChild(interestInfo);
+        expandButton.appendChild(downArrow);
+
+
+        chatBox.appendChild(chatBoxRow);
+        chatBox.appendChild(expandButton);
 
         document.getElementById('chatBoxes').appendChild(chatBox);
     //     </div>
@@ -64,9 +85,9 @@ $(document).ready(function() {
 /*$('.chatBox').click(function(e) {
     console.log($(e.currentTarget).attr("data-id"));
 });*/
-$('div.chatBoxes').on('click', 'div.chatBox', function(e) {
+/*$('div.chatBoxes').on('click', 'div.chatBox', function(e) {
     if($(e.currentTarget).attr("style") === 'height: 300px;'){
-        $(e.currentTarget).animate({height: "100px"});
+        $(e.currentTarget).animate({height: "121px"});
         var interestLastMessage = document.createElement("DIV");
         interestLastMessage.setAttribute("class","interestLastMessage");
         var chatText = document.createElement("P");
@@ -92,6 +113,20 @@ $('div.chatBoxes').on('click', 'div.chatBox', function(e) {
 
     }
 
+});*/
+$('div.chatBoxes').on('click', 'button.expandButton', function(e) {
+// $("button.expandButton").click(function() {
+    if ($(this).parent().hasClass('expand')) {
+        $(this).parent().removeClass('expand')
+        $(this).find("i.up").attr("class","down");
+        $(this).find("i.up").remove();
+    }
+    else {
+        $(this).parent().addClass('expand')
+        $(this).find("i.down").attr("class","up");
+        $(this).find("i.down").remove();
+
+    }
 });
 
 /*
