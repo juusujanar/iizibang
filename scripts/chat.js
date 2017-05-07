88db693c7a55ddfb056e093e8b16e50364903d99
Cardo Kambla <kamblaca@gmail.com>
2017-05-07 11:40:57 +0300

    Test if collapsing returns last message

--- scripts/chat.js
+++ scripts/chat.js
@@ -110,15 +110,46 @@ $('div.chatBoxes').on('click', 'button.expandButton', function (e) {
         $(this).find("i.up").remove();
         $(this).parent().find('div.messageBox').remove();
         $(this).parent().find("div.messageBox").removeClass('expand');
-        var chatText = document.createElement("P");
-        chatText.setAttribute("class", "chatTextMiddle");
-        if (lastMessage === "") {
-            chatText.innerHTML = "Say Hello!";
-        } else {
-            chatText.innerHTML = lastMessage;
-        }
+        $.ajax({
+            url: 'https://iizibang.jjdev.eu/api/successfulmatches',
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                matches = data;
+                console.log(data);
+                showMatches(matches);
+                for (var i = 0; i < matches.length; i++) {
+                    var matchID = matches[i].id;
+                    $.ajax({
+                        url: 'https://iizibang.jjdev.eu/api/chathistory?matchid='+matchID,
+                        type: 'GET',
+                        contentType: "application/json; charset=utf-8",
+                        success: function (data) {
+                            chatMessages = data;
+                            console.log(data);
+                            var searchID = "#"+matchID;
+                            var lastMessage = chatMessages[chatMessages.length-1];
+                            var chatText = document.createElement("P");
+                            chatText.setAttribute("class", "chatTextMiddle");
+                            if (data.length <= 0) {
+                                chatText.innerHTML = "Say Hello!";
+                            } else {
+                                chatText.innerHTML = lastMessage.text;
+                            }
+                            $(searchID).append(chatText)
 
-        $(this).parent().find('div.interestInfo').append(chatText);
+
+                        },
+                        error: function (data) {
+                            console.log(data);
+                        }
+                    });
+                }
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
         $(this).parent().find('.userInputPlacement').remove();
     }
     else {
