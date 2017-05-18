bf5c25328e4d5263881de7c7bc532b6ab8467b75
Janar Juusu <janar.juusu@gmail.com>
2017-05-19 01:05:52 +0300

    Switch to minified files + minor fixes in failover.js

--- scripts/failover.js
+++ scripts/failover.js
@@ -1,10 +1,9 @@
-window.jQuery || document.write('<script src="js/jquery-3.1.1.min.js">\x3C/script>');
+window.jQuery || document.write('<script src="scripts/jquery-3.1.1.min.js">\x3C/script>');
 
 function sendHome(){
     window.location.replace("https://iizibang.jjdev.eu/");
 }
 
 function sendToLogin(){
-    window.location.replace("https://iizibang.jjdev.eu//login/index.html");
+    window.location.replace("https://iizibang.jjdev.eu/login/index.html");
 }
-
